23435dd6574b91c059bead6db35e49b77e362d84
LeandroAgustinFernandez <leandroa.fernandez@gmail.com>
2024-12-16 07:39:13 -0300

    Fix upload file from web

--- components/DownloadLink.js
+++ components/DownloadLink.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import { Text, StyleSheet, Pressable, Platform, Linking, Alert } from 'react-native';
-import * as FileSystem from 'expo-file-system';
-import * as Sharing from 'expo-sharing';
+import React from "react";
+import { Text, StyleSheet, Pressable, Platform, Linking, Alert } from "react-native";
+import * as FileSystem from "expo-file-system";
+import * as Sharing from "expo-sharing";
 
 const DownloadLink = ({ file }) => {
   const handleDownload = async () => {
     if (!file || !file.uri) {
-      Alert.alert('Error', 'No se proporcionó ningún archivo para descargar.');
+      Alert.alert("Error", "No se proporcionó ningún archivo para descargar.");
       return;
     }
 
     try {
-      if (Platform.OS === 'ios' || Platform.OS === 'android') {
-        const fileUri = `${FileSystem.documentDirectory}${file.name || 'download'}`;
-        console.log(file.uri);
-        console.log(fileUri);       
+      if (Platform.OS === "ios" || Platform.OS === "android") {
+        const fileUri = `${FileSystem.documentDirectory}${file.name || "download"}`;        
         const { uri } = await FileSystem.downloadAsync(file.uri, fileUri);
-        console.log(uri);
-        
         if (await Sharing.isAvailableAsync()) {
           await Sharing.shareAsync(uri);
         } else {
-          Alert.alert('Descarga completada', `Archivo guardado en: ${uri}`);
+          Alert.alert("Descarga completada", `Archivo guardado en: ${uri}`);
         }
-      } 
-      else if (Platform.OS === 'web') {
-        const link = document.createElement('a');
+      } else if (Platform.OS === "web") {
+        const link = document.createElement("a");
         link.href = file.uri;
-        link.download = file.name || 'download';
-        link.click();
+
+        window.open(file.uri, "_blank");
       }
     } catch (error) {
-      console.error('Error downloading file:', error);
-      Alert.alert('Error', 'Hubo un problema al descargar el archivo.');
+      console.error("Error downloading file:", error);
+      Alert.alert("Error", "Hubo un problema al descargar el archivo.");
     }
   };
 
@@ -47,8 +42,8 @@ const DownloadLink = ({ file }) => {
 
 const styles = StyleSheet.create({
   link: {
-    color: '#007BFF',
-    textDecorationLine: 'underline',
+    color: "#007BFF",
+    textDecorationLine: "underline",
     fontSize: 16,
     marginVertical: 8,
   },
--- screens/Studies/StudiesForm.js
+++ screens/Studies/StudiesForm.js
@@ -1,247 +1,3 @@
-// import React, { useState, useContext } from "react";
-// import {
-//   Text,
-//   TextInput,
-//   TouchableOpacity,
-//   StyleSheet,
-//   ScrollView,
-//   SafeAreaView,
-//   Alert,
-//   Platform,
-// } from "react-native";
-// import { UserContext } from "../../context/UserContext";
-// import { saveStudy, updateStudy } from "../../services/api";
-// import Header from "../../components/Header";
-// import * as DocumentPicker from "expo-document-picker";
-// import DateTimePicker from "@react-native-community/datetimepicker";
-// import DateTimePickerModal from "react-native-modal-datetime-picker";
-
-// const cloudinaryUrl = "https://api.cloudinary.com/v1_1/dajilyvt7/";
-// const uploadPreset = "damu_uploads";
-
-// const StudiesForm = ({ navigation, route }) => {
-//   const { user } = useContext(UserContext);
-//   const { study } = route.params || {};
-//   const [showDatePicker, setShowDatePicker] = useState(false);
-
-//   const [formData, setFormData] = useState({
-//     name: study?.name || "",
-//     date: study?.date ? new Date(study.date) : new Date(),
-//     doctor_name: study?.doctor_name || "",
-//     additionale_info: study?.additionale_info || "",
-//     file: study?.file_name
-//       ? { name: study.file_name, uri: study?.file_url }
-//       : "",
-//   });
-
-//   const onChangeDate = (event, selectedDate) => {
-//     const currentDate = selectedDate || formData.date;
-//     setShowDatePicker(false);
-//     handleInputChange("date", currentDate);
-//   };
-
-//   const handleInputChange = (field, value) => {
-//     setFormData({ ...formData, [field]: value });
-//   };
-
-// const handleFilePicker = async () => {
-//   try {
-//     const result = await DocumentPicker.getDocumentAsync({ type: "*/*" });
-//     console.log(result);
-//     if (Platform.OS === "web") {
-//       if (result && !result.canceled)
-//         setFormData({ ...formData, file: result });
-//     } else {
-//       if (!result.canceled) {
-//         let selectedFile = null;
-//         if (result.assets && result.assets.length > 0) {
-//           selectedFile = result.assets[0];
-//         } else {
-//           selectedFile = result;
-//         }
-//         if (selectedFile) {
-//           setFormData({ ...formData, file: selectedFile });
-//         }
-//       }
-//     }
-//   } catch (error) {
-//     console.error("Error picking document:", error);
-//   }
-// };
-
-//   const uploadFileToCloudinary = async (file) => {
-//     if (!file) return null;
-
-//     const data = new FormData();
-//     data.append("file", {
-//       uri: file.uri,
-//       type: file.mimeType,
-//       name: file.name.replace(/[#-]/g, ""),
-//     });
-//     let path = "upload";
-//     if (file.mimeType == "application/pdf") {
-//       data.append("resource_type", "raw");
-//       path = "raw/upload";
-//     }
-//     data.append("upload_preset", uploadPreset);
-
-//     console.log(cloudinaryUrl + path);
-
-//     try {
-//       const response = await fetch(cloudinaryUrl + path, {
-//         method: "POST",
-//         body: data,
-//         headers: {
-//           "Content-Type": "multipart/form-data",
-//         },
-//       });
-//       const result = await response.json();
-//       console.log(result);
-//       return result.secure_url; // URL del archivo subido
-//     } catch (error) {
-//       console.error("Error uploading file:", error);
-//       Alert.alert("Error", "Error uploading file to Cloudinary");
-//       return null;
-//     }
-//   };
-
-//   const handleSaveStudy = async () => {
-//     try {
-//       let fileUrl = "";
-//       if (study && study?.file_name == formData.file.name) {
-//         console.log("Entro con un estudio");
-//         console.log(formData.file);
-
-//         fileUrl = study.file_url;
-//       } else {
-//         console.log("Entro con un estudio nuevo");
-//         fileUrl = formData.file
-//           ? await uploadFileToCloudinary(formData.file)
-//           : study?.file_url || "";
-//       }
-
-//       const studyData = {
-//         ...formData,
-//         user_id: user.user_id,
-//         file_name: formData.file.name.replace(/[#-]/g, ""),
-//         file_url: fileUrl,
-//       };
-
-//       if (study) {
-//         await updateStudy(study.id, studyData);
-//       } else {
-//         await saveStudy(studyData);
-//       }
-
-//       navigation.goBack();
-//     } catch (error) {
-//       console.error("Error saving study:", error);
-//       Alert.alert("Error", "Failed to save study");
-//     }
-//   };
-
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <Header
-//         title={study ? "Editar estudio" : "Nuevo estudio"}
-//         onClose={() => navigation.goBack()}
-//         bgColor="#F7931E"
-//       />
-//       <ScrollView contentContainerStyle={styles.form}>
-//         <TouchableOpacity
-//           onPress={() => setShowDatePicker(true)}
-//           style={styles.input}
-//         >
-//           <Text style={styles.inputText}>
-//             {formData.date.toLocaleDateString()}
-//           </Text>
-//         </TouchableOpacity>
-
-//         {showDatePicker && (
-//           <DateTimePicker
-//             value={formData.date}
-//             mode="date"
-//             display="default"
-//             onChange={onChangeDate}
-//           />
-//         )}
-
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Nombre del estudio"
-//           value={formData.name}
-//           onChangeText={(value) => handleInputChange("name", value)}
-//         />
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Indicado por"
-//           value={formData.doctor_name}
-//           onChangeText={(value) => handleInputChange("doctor_name", value)}
-//         />
-//         <TouchableOpacity style={styles.fileButton} onPress={handleFilePicker}>
-//           <Text style={styles.fileButtonText}>
-//             {formData.file ? formData.file.name : "Seleccionar archivo"}
-//           </Text>
-//         </TouchableOpacity>
-//         <TextInput
-//           style={styles.input}
-//           placeholder="Otros datos"
-//           value={formData.additionale_info}
-//           onChangeText={(value) => handleInputChange("additionale_info", value)}
-//         />
-//         <TouchableOpacity style={styles.saveButton} onPress={handleSaveStudy}>
-//           <Text style={styles.saveButtonText}>Guardar</Text>
-//         </TouchableOpacity>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#f0f0f0",
-//   },
-//   form: {
-//     padding: 20,
-//   },
-//   input: {
-//     backgroundColor: "#fff",
-//     padding: 15,
-//     borderRadius: 10,
-//     elevation: 5,
-//     shadowColor: "#000",
-//     shadowOffset: { width: 0, height: 2 },
-//     shadowOpacity: 0.2,
-//     shadowRadius: 5,
-//     marginBottom: 15,
-//   },
-//   fileButton: {
-//     backgroundColor: "#FF8C00",
-//     padding: 15,
-//     borderRadius: 10,
-//     alignItems: "center",
-//     marginBottom: 15,
-//   },
-//   fileButtonText: {
-//     color: "#fff",
-//     fontSize: 16,
-//   },
-//   saveButton: {
-//     backgroundColor: "#FF8C00",
-//     padding: 15,
-//     borderRadius: 10,
-//     alignItems: "center",
-//   },
-//   saveButtonText: {
-//     color: "#fff",
-//     fontSize: 18,
-//     fontWeight: "bold",
-//   },
-// });
-
-// export default StudiesForm;
-
 import React, { useState, useContext } from "react";
 import {
   Text,
@@ -291,10 +47,6 @@ const StudiesForm = ({ navigation, route }) => {
     try {
       const result = await DocumentPicker.getDocumentAsync({ type: "*/*" });
       console.log(result);
-      // if (Platform.OS === "web") {
-      //   if (result && !result.canceled)
-      //     setFormData({ ...formData, file: result });
-      // } else {
       if (!result.canceled) {
         let selectedFile = null;
         if (result.assets && result.assets.length > 0) {
@@ -305,7 +57,6 @@ const StudiesForm = ({ navigation, route }) => {
         if (selectedFile) {
           setFormData({ ...formData, file: selectedFile });
         }
-        // }
       }
     } catch (error) {
       console.error("Error picking document:", error);
@@ -316,30 +67,36 @@ const StudiesForm = ({ navigation, route }) => {
     if (!file) return null;
 
     const data = new FormData();
-    data.append("file", {
-      uri: file.uri,
-      type: file.mimeType,
-      name: file.name.replace(/[#-]/g, ""),
-    });
+
+    if (Platform.OS === "web") {
+      const fileBlob = await fetch(file.uri).then((res) => res.blob());
+      data.append("file", fileBlob, file.name.replace(/[#-]/g, ""));
+    } else {
+      data.append("file", {
+        uri: file.uri,
+        type: file.mimeType || "application/octet-stream",
+        name: file.name.replace(/[#-]/g, ""),
+      });
+    }
+
     let path = "upload";
-    if (file.mimeType == "application/pdf") {
+    if (file.mimeType === "application/pdf") {
       data.append("resource_type", "raw");
       path = "raw/upload";
     }
     data.append("upload_preset", uploadPreset);
 
-    console.log(cloudinaryUrl + path);
-
     try {
       const response = await fetch(cloudinaryUrl + path, {
         method: "POST",
         body: data,
-        // headers: {
-        //   "Content-Type": "multipart/form-data",
-        // },
       });
       const result = await response.json();
-      console.log(result);
+
+      if (result.error) {
+        throw new Error(result.error.message);
+      }
+
       return result.secure_url;
     } catch (error) {
       console.error("Error uploading file:", error);
@@ -387,9 +144,7 @@ const StudiesForm = ({ navigation, route }) => {
         bgColor="#F7931E"
       />
       <ScrollView contentContainerStyle={styles.form}>
-        {/* Campo de fecha */}
         {Platform.OS === "web" ? (
-          // <TouchableOpacity style={styles.input}>
           <Text style={[styles.input, styles.inputSelect]}>
             <input
               type="date"
@@ -401,7 +156,6 @@ const StudiesForm = ({ navigation, route }) => {
             />
           </Text>
         ) : (
-          // </TouchableOpacity>
           <>
             <TouchableOpacity
               onPress={() => setShowDatePicker(true)}
